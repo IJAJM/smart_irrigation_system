9874d606d398209099d6bb380a126f0e8c078f98
Ijajkeyboard <50859787+IJAJM@users.noreply.github.com>
2025-03-15 00:47:34 +0700

    Update pump_control.py

--- control/pump_control.py
+++ control/pump_control.py
@@ -1,1 +1,26 @@
-# TODO: Implement pump_control.py
+import RPi.GPIO as GPIO
+import time
+from sensors.read_sensors import read_sensors
+
+PUMP_PIN = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PUMP_PIN, GPIO.OUT)
+
+def control_pump():
+    data = read_sensors()
+    if data["soil_moisture"] == 0:
+        print("Tanah kering, menyalakan pompa...")
+        GPIO.output(PUMP_PIN, GPIO.HIGH)
+        time.sleep(10)
+        GPIO.output(PUMP_PIN, GPIO.LOW)
+        print("Pompa dimatikan.")
+    else:
+        print("Tanah cukup lembab.")
+
+if __name__ == "__main__":
+    try:
+        while True:
+            control_pump()
+            time.sleep(30)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
